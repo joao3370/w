83217db90b0be699fbb7005ef276266c0f606991
joao3370 <sidilokao9@gmail.com>
2021-03-03 06:03:25 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,36 @@
+instalação 
+
+COMANDOS:
+
+
+pkg update && pkg upgrade
+
+pkg install git
+
+pkg install wget
+
+git clone https://github.com/joao3370/w.git
+
+ls
+
+cd w
+
+ls
+
+pkg install unzip
+
+pkg install nodejs
+
+pkg install ffmpeg
+
+pkg install tesseract
+
+unzip w.zip
+
+cd w
+
+ls
+
+bash install.sh
+
+npm start
---
+++ src/iklan.js
@@ -0,0 +1,18 @@
+const iklan = () => { 
+	return `           
+╔══✪〘 IKLAN 〙✪══
+║
+╠═══════════════════════════
+╠➥ *LISTA DE ALUGUEL E CRIAR BOTS:*
+╠➥ *ALUGUEL: 0/ GRUPO (MÊS)*
+╠➥ *CRIAR: 0 (PODE SER PROPRIETÁRIO)*
+╠➥ *PODE PAGAR ATRAVÉS DE:*
+╠➥ *PIC PAY,*
+╠═══════════════════════════
+╠➥ *VANTAGENS*
+╠➥ *wa.me/5548991635455*
+║
+╚═〘 JOAO BOT 〙
+`
+}
+exports.iklan = iklan
